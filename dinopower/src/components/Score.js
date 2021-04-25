3fa8cea00788d34b077c40830a63000923d93743
sierraog <sierrao@utexas.edu>
2021-04-25 16:11:37 -0500

    added local storage highscore

--- dinopower/src/components/Score.js
+++ dinopower/src/components/Score.js
@@ -2,11 +2,20 @@ import React, {useState, useEffect } from 'react';
 
 export default function Score({gameStarted, gamePaused, gameRestarted}) {
     const [score, setScore] = useState(0);
-    const [highscore, setHighScore] = useState(0);
+
+    localStorage.getItem('myData');
+    useEffect(() => {
+        if (!localStorage.getItem('highscore')){
+            localStorage.setItem('highscore', 0);
+        }
+        if (score > localStorage.getItem('highscore')) {
+            localStorage.setItem('highscore', score);
+        }
+    }, [score]);
 
     return(
         <div className="Score">
-            <p>Score: {score}, Highscore: {highscore}</p>
+            <p>Score: {score}, Highscore: {localStorage.getItem('highscore')}</p>
         </div>
     )
 }
