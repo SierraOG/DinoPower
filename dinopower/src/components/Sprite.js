9ac5f3917055584dc7a2acc0231caf58dc2a61cc
sierraog <sierrao@utexas.edu>
2021-04-29 08:57:14 -0500

    sprite animation example working

--- dinopower/src/components/Sprite.js
+++ dinopower/src/components/Sprite.js
@@ -1,24 +1,35 @@
-import React, {useState, useEffect, setInterval } from 'react';
-import bug1 from '../img/img_01.png';
-import bug2 from '../img/img_02.png';
+import React, {useState, useEffect } from 'react';
+import sp1 from '../img/1.png';
+import sp2 from '../img/2.png';
+import sp3 from '../img/3.png';
+import sp4 from '../img/4.png';
+import sp5 from '../img/5.png';
+import sp6 from '../img/6.png';
+import sp7 from '../img/7.png';
+import sp8 from '../img/8.png';
 
-imgArray = [bug1, bug2];
-frameCount = 2;
 export default function Sprite({gamePaused}) {
     const [frame, setFrame] = useState(0);
+    const imgArray = [sp1, sp2, sp3, sp4, sp5, sp6, sp7, sp8];
+    const frameCount = 8;
 
-    setInterval(() => {
-        if (!gamePaused){
-            setFrame(frame + 1);
-            if (frame >= frameCount) {
-                setFrame(0)
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // console.log(frame);
+            // console.log(gamePaused);
+            if (!gamePaused){
+                setFrame(frame + 1);
+                if (frame >= frameCount - 1) {
+                    setFrame(0);
+                }
             }
-        }
-    }, 300);
+        }, 100);
+        return () => clearInterval(interval);
+    }, [frame, gamePaused]);
 
     return(
         <div>
-            <img src={imgArray[frame]}/>
+            <img src={imgArray[frame]} alt="sprite"/>
         </div>
     )
 }
