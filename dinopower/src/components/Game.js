a058e94c117ec825cd692f3194191e7ca44a4192
Jake Goins <jakegoins@utexas.edu>
2021-04-29 20:00:49 -0500

    Working on percentages

--- dinopower/src/components/Game.js
+++ dinopower/src/components/Game.js
@@ -1,42 +1,48 @@
 import React, {useState, useEffect } from 'react';
 import Score from './Score';
 import Sprite from './Sprite';
+import Character from './Character';
 
 export default function Game({gameStarted, gamePaused, gameRestarted}) {
     const [gameOver, setGameOver] = useState(false);
+    const [y,setY] = useState(0);
+    const [x,setX] = useState(0);
 
     const handleKeyPress = (event) => {
         switch (event.keyCode) {
             case 37:
                 console.log('left');
+                setX(x => (x-10));
                 break;
             case 38:
                 console.log('up');
+                setY(y => (y-10));
                 break;
             case 39:
                 console.log('right');
+                setX(x => (x+10));
                 break;
             case 40:
                 console.log('down');
+                setY(y => (y+10));
                 break;
         }
     }
 
     useEffect(() => {
         document.addEventListener("keydown", handleKeyPress, false);
-    
+
         return () => {
-          document.removeEventListener("keydown", handleKeyPress, false);
+            document.removeEventListener("keydown", handleKeyPress, false);
         };
     }, []);
 
     return(
         <div className="Game">
             <Score gameStarted={gameStarted} gamePaused={gamePaused} gameRestarted={gameRestarted}/>
-            <svg width="60vw" height="60vh" className="GameCanvas">
-                <rect x="0" y="0" width="100%" height="100%"/>
-            </svg>
-            <Sprite gamePaused={gamePaused}/>
+            <div className="GameCanvas">
+                <Character x = {x} y = {y} z = {100} gamePaused = {gamePaused}/>
+            </div>
         </div>
     )
 }
